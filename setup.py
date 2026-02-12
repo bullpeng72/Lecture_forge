c6560b54f5a422a12f2f91473a5837bd7852e64c
Sungwoo Kim <sungwoo.kim@gmail.com>
2026-02-12 18:23:47 +0900

    푸시

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
             "templates/*.html",
             "templates/*.css",
             "templates/*.js",
+            "templates/prompts/*.txt",
             ".env.example",
         ],
     },
